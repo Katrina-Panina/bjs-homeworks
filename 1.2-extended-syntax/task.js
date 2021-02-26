ef7ccc921b1d994fabc0a1ab051e0818b58a5c2a
Katerina Panina <khomenko.kat@mail.ru>
2021-02-26 16:55:28 +0300

    second commit wrappers-for-primitives

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -7,17 +7,17 @@ function getResult(a,b,c){
     let d = b**2 - 4 * a * c; // Дискриминант
    
     if (d < 0) {
-       
-        console.log("x не имеет значения.");
+        return x;
+        //console.log("x не имеет значения.");
     }
     else if (d > 0) {
         x.push((-b + Math.sqrt(d)) / (2 * a));
         x.push((-b - Math.sqrt(d)) / (2 * a));
-        console.log("х имеет 2 значения.");  
+        //console.log("х имеет 2 значения.");  
     }
-    else if (d == 0) {
+    else if (d === 0) {
         x.push((-b + Math.sqrt(d)) / (2 * a));
-        console.log("х имеет 1 значение.");  
+        //console.log("х имеет 1 значение.");  
         }
 
      return x;
@@ -26,39 +26,30 @@ function getResult(a,b,c){
 function getAverageMark(marks) {
  
     let sum = 0;
-    
+     marks.splice(5);
+
     if (marks.length == 0) {
-        console.log("Ошибка формата!");
         return 0;
-    } else if (marks.length > 5 ) {
-        marks.splice(5);
-        console.log("Вы хотите ввести больше оценок, чем возможно - " + marks);
-      }   
-    
-      for(let i = 0; i < marks.length; i++){
+    } 
+
+     for(let i = 0; i < marks.length; i++){
         sum += marks[i];
         }
-    console.log(sum);
-
-    let getAverageMark = sum/marks.length; //среднее значение.
-
-     return getAverageMark;
+   
+     return sum/marks.length;
 }
 
 
 function askDrink(name,dateOfBirthday){
    
-    let years = ateOfBirthday.getFullYear();
-    //years == result;
-    let d = new Date();
-    console.log(d);
-
-    if (years < 2003) {
-        console.log("Не желаете ли олд-фэшн, " + name + "?" );
+    let years = dateOfBirthday.getFullYear();
+    let d = new Date().getFullYear();
+   
+    if ((d-years) >= 18) {
+        return (`Не желаете ли олд-фэшн, ${name}?`);
+    } else if  ((d-years) < 18) {
+        return (`Сожалею, ${name},но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`);
     }
-    else if (years < 2003) {
-        console.log("Сожалею, " + name + ", но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!" );
-    }
-
-     return years;
+    
+    //return
 }
