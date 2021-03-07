294fef958738b1c59eb86a90c88d560c57338cbd
Katerina Panina <khomenko.kat@mail.ru>
2021-03-07 18:28:11 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,38 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict"; // код  обрабатывается в строгом режиме
+//Задача №1 2.2 «ПРОТОТИП И КОНСТРУКТОР ОБЪЕКТА»
 
+String.prototype.screening = function isPalindrome(str){
+  
+    let res = this.str.toLowerCase();
+    res = strLen;
+
+   let strLen = str.length,
+    strReverse = str.split('').reverse().join(''); 
+
+    if (strReverse == str) {
+    return 'true';
+
+    } else {
+     return 'false';
+ 
+  }
+ }
+test = isPalindrome('abcdedcba');
+test2 = isPalindrome('abcded');
+test3 = ('А роза упала на лапу Азора')//true
+
+//Задача №2 2.2 «ПРОТОТИП И КОНСТРУКТОР ОБЪЕКТА»
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) return 0;
+
+    const average =marks.reduce((partial_sum, a) => partial_sum + a, 0) / marks.length;
+    const roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
+//Задача №3 2.2 «ПРОТОТИП И КОНСТРУКТОР ОБЪЕКТА»
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
+   let now = 
+   
     // return verdict
 }
